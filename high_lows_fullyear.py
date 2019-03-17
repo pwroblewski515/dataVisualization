d2cfdd0df10d94d322012f51144e4f9aff708eaa
Paul <p.wroblewski515@gmail.com>
2019-03-17 13:06:05 -0500

    adding the rest of the files that I forgot to add

---
+++ high_lows_fullyear.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# Get the dates, low, and high temperatures from the file
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	dates, highs, lows = [], [], []
+	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y-%m-%d")
+		dates.append(current_date)
+
+		high = int(row[1])
+		highs.append(high)
+
+		low = int(row[3])
+		lows.append(low)
+
+# plot data
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
+
+# Format plot
+plt.title("Daily High Temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
